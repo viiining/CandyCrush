0f803d211f18233ced12417bf4145d2f974cca47
Zinni Chang <czinni853@gmail.com>
2023-06-27 18:41:29 +0800

    add candy-007

---
+++ candy-007.js
@@ -0,0 +1,15 @@
+// 編號：CANDY-007
+// 程式語言：JavaScript
+// 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
+
+function findSomeDifferent(numbers) {
+  let oddElements = numbers.filter((el) => el % 2 == 1);
+  let evenElements = numbers.filter((el) => el % 2 == 0);
+  return Number(oddElements.length > 1 ? evenElements : oddElements);
+}
+
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])); // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])); // 印出 160
+
+// solving steps:
+// 宣告兩個變數各自存有奇數與偶數元素的陣列，並以陣列長度判斷，如果奇數陣列的長度有超過一個，表示這陣列大部分是奇數，那我就要印出不是奇數的那個數值，反之亦然。
