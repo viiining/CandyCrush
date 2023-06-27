0a5f41b2a3c1e163aa540a88b8b6e453bead8bb2
Zinni Chang <czinni853@gmail.com>
2023-06-27 17:33:37 +0800

    make the revision of candy-005

--- candy-005.js
+++ candy-005.js
@@ -16,3 +16,17 @@ console.log(squareDigits(387)); // 印出 96449
 
 // solving steps:
 // 先建立一個空字串，並將數字轉為字串之後，以 for 迴圈抓取每個字做轉換數字，接著平方，最後加進一開始建立的空字串中，輸出答案。
+
+// 編號：CANDY-005 -- revision
+function squareDigits(num) {
+  return String(num)
+    .split("")
+    .map((digit) => parseInt(digit) ** 2)
+    .join("");
+}
+console.log(squareDigits(3212)); // 印出 9414
+console.log(squareDigits(2112)); // 印出 4114
+console.log(squareDigits(387)); // 印出 96449
+
+// solving steps:
+// 先將數字轉為字串型態，並以split(“”)方法達成用“”分開每個字串並成為一個陣列，接著用map()對每個元素轉成整數之後做平方，最後再以join("")可以讓array轉為字串，輸出答案。
