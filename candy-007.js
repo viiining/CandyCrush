e908c21ff86c8dca2aa317101a49ae358652ecc4
Zinni Chang <czinni853@gmail.com>
2023-06-28 10:31:49 +0800

    revise the error of using Number() and merge line 20 and 21 into one

--- candy-007.js
+++ candy-007.js
@@ -13,3 +13,24 @@ console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])); // 印出 160
 
 // solving steps:
 // 宣告兩個變數各自存有奇數與偶數元素的陣列，並以陣列長度判斷，如果奇數陣列的長度有超過一個，表示這陣列大部分是奇數，那我就要印出不是奇數的那個數值，反之亦然。
+
+// CANDY-007 --- revision 針對誤用 Number() 做修正！
+function findSomeDifferent(numbers) {
+  let oddElements = numbers.filter((el) => el % 2 == 1);
+  let evenElements = numbers.filter((el) => el % 2 == 0);
+  let parseResult = oddElements.length > 1 ? evenElements : oddElements;
+  return parseResult[0];
+}
+
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])); // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])); // 印出 160
+
+// 編號：CANDY-007 --- revision01（不要取多餘的代名詞）
+function findSomeDifferent(numbers) {
+  let oddElements = numbers.filter((el) => el % 2 == 1);
+  let evenElements = numbers.filter((el) => el % 2 == 0);
+  return (oddElements.length > 1 ? evenElements : oddElements)[0];
+}
+
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])); // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])); // 印出 160
