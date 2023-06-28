92ad0b91a3a18fa842df54f6f2b0bcaf3557bc5a
Zinni Chang <czinni853@gmail.com>
2023-06-28 14:47:44 +0800

    optimize the first crush by using regular expression and revise the description of solving steps and review the mistake

--- candy-009.js
+++ candy-009.js
@@ -12,3 +12,19 @@ console.log(removeAnchor("5xruby.tw/courses/?page=1#about")); // 印出 5xruby.t
 
 // solving steps:
 // 以三元運算子去判斷 url 是否包含 "#about" 字串，是的話用 replace() 方法將空字串取代 "#about"；否則回傳 url 本身。
+
+// 編號：CANDY-009 -- revision
+// 程式語言：JavaScript
+// 題目：移除網址中的錨點（Anchor）
+
+function removeAnchor(url) {
+  return url.replace(/\#[a-z]*/, "");
+}
+
+console.log(removeAnchor("5xruby.tw")); // 印出 5xruby.tw
+console.log(removeAnchor("5xruby.tw/#about")); // 印出 5xruby.tw/
+console.log(removeAnchor("5xruby.tw/courses/?page=1#about")); // 印出 5xruby.tw/courses/?page=1
+
+// solving steps:
+// 以常規表示法，去判斷 url 是否包含 "#與a-z"，是的話用 replace() 方法將空字串取代。
+// 先前解題時，以常規表示法試過而失敗（當時沒有持續探究原因）轉而以答案解題 使用 "#about"（如此會造成此方法僅適用此題目 That's not good.）
