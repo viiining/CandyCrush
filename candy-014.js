e6b15b286855f9f012823a1bc348f4fe4e70e24f
Zinni Chang <czinni853@gmail.com>
2023-07-02 19:11:59 +0800

    optimize the first crush by using different thoughts and revise the description of solving steps

--- candy-014.js
+++ candy-014.js
@@ -20,3 +20,22 @@ console.log(uniqueOrder([1, 1, 1, 2, 2, 2, 1])); // [1, 2, 1]
 
 // solving steps:
 // 判斷前一項不等於後一項時，回傳至新陣列。
+
+// 編號：CANDY-014 --- revision
+// 程式語言：JavaScript
+// 題目：把鄰近的重複值去除，但仍照原本的順序排序
+// 範例："AAABBBDDDAABBBCC" -> ['A', 'B', 'D', 'A', 'B', 'C']
+
+function uniqueOrder(sequence) {
+  return Array.from(sequence).filter(
+    (element, index, array) => element !== array[index + 1]
+  );
+}
+
+console.log(uniqueOrder("AABCC")); // [ 'A', 'B', 'C']
+console.log(uniqueOrder("AAABBBCCBCC")); // [ 'A', 'B', 'C', 'B', 'C']
+console.log(uniqueOrder([1, 2, 1, 2, 1])); // [ 1, 2, 1, 2, 1 ]
+console.log(uniqueOrder([1, 1, 1, 2, 2, 2, 1])); // [1, 2, 1]
+
+// solving steps:
+// 以 filter 判斷 element 不等於後一項時，回傳至新陣列。
