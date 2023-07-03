0ce3004298b9e0317fd31b12281efecb70abfecb
Zinni Chang <czinni853@gmail.com>
2023-07-03 21:24:23 +0800

    optimize the first crush by using different thoughts and revise the description of solving steps

--- candy-017.js
+++ candy-017.js
@@ -16,3 +16,23 @@ console.log(countBits(9527)); // 8
 
 // solving steps:
 // 先將 num 轉為二進位，以 split("") 轉為陣列之後，以 reduce() 累加的方式去計算有幾個 1 （因為二進位制只有 0 跟 1 可以用累加直接計算）＊＊其中要注意為 cv 需轉回 10 進位制累加＊＊
+
+// 編號：CANDY-017 --- revision
+// 程式語言：JavaScript
+// 題目：計算數字的 2 進位裡有幾個 1
+// 範例：5 -> 101 -> 2 個 1
+
+function countBits(num) {
+  return num
+    .toString(2)
+    .split("")
+    .filter((element) => element === "1").length;
+}
+
+console.log(countBits(1234)); // 5
+console.log(countBits(1450)); // 6
+console.log(countBits(9527)); // 8
+
+// solving steps:
+// 先將 num 轉為二進位，以 split("") 轉為陣列之後，以 filter() 過濾只有 1 的陣列並回傳其長度。
+// 此法相較之前更直覺也更易懂～
