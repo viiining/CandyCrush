9154a051659bc26bb441f5ef0a29af4fe29967f3
Zinni Chang <czinni853@gmail.com>
2023-07-03 22:20:34 +0800

    optimize the first crush by using different thoughts and revise the description of solving steps

--- candy-018.js
+++ candy-018.js
@@ -15,3 +15,19 @@ console.log(randomNumber(5, 30)); // 隨機印出 5 ~ 29 之間的任何一個�
 // 用 Math.random() 方法可以隨機亂數回傳想要的數字區間。
 // 因為代入的參數有一個或兩個，可以先判斷如果 arg[1] 是 undefined (意即只有一個參數代入)，則直接以 Math.floor 無條件捨去取整數。
 // 若是有兩個參數，讓值回傳區間在 第一個參數 ～（第二個參數-1）
+
+// 編號：CANDY-018 --- revision
+// 程式語言：JavaScript
+// 題目：實作一個可以印出隨機整數的函數
+
+function randomNumber(...args) {
+  return args.length === 1
+    ? Math.floor(Math.random() * args[0])
+    : Math.floor(Math.random() * (args[1] - args[0]) + args[0]);
+}
+
+console.log(randomNumber(50)); // 隨機印出 0 ~ 49 之間的任何一個數字
+console.log(randomNumber(5, 30)); // 隨機印出 5 ~ 29 之間的任何一個數字
+// solving steps:
+// 以 args 的長度去判斷，如果長度是 1 ，則回傳一個介於 0（包含）和 args[0]（不包含）之間的隨機整數；
+// 其餘則回傳 args[0]（包含）和 args[1]（不包含）之間的隨機整數
