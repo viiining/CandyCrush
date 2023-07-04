2a97426684f8d40f29376c0cd4d762869a00832e
Zinni Chang <czinni853@gmail.com>
2023-07-04 15:34:25 +0800

    optimize the first revision

--- candy-018.js
+++ candy-018.js
@@ -31,3 +31,16 @@ console.log(randomNumber(5, 30)); // 隨機印出 5 ~ 29 之間的任何一個�
 // solving steps:
 // 以 args 的長度去判斷，如果長度是 1 ，則回傳一個介於 0（包含）和 args[0]（不包含）之間的隨機整數；
 // 其餘則回傳 args[0]（包含）和 args[1]（不包含）之間的隨機整數
+
+// 編號：CANDY-018 --- revision01
+// 程式語言：JavaScript
+// 題目：實作一個可以印出隨機整數的函數
+
+function randomNumber(min = 0, max = 0) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(randomNumber(50)); // 隨機印出 0 ~ 49 之間的任何一個數字
+console.log(randomNumber(5, 30)); // 隨機印出 5 ~ 29 之間的任何一個數字
+// solving steps:
+// 給定兩個參數預設值都為 0，這樣可以避開如果只有一個參數傳進來時的報錯，回傳 min（包含）和 max（不包含）之間的隨機整數。
