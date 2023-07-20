8caee77c70ea022a646699aade0632e2c3d80d6a
Zinni Chang <czinni853@gmail.com>
2023-07-20 08:41:49 +0800

    add the solving steps

--- CandyCrush.rb/candy-010.rb
+++ CandyCrush.rb/candy-010.rb
@@ -6,13 +6,13 @@
 def expandedForm(num)
   return num.to_s if num < 10
 
-  multiplesOfTen = 10**(num.to_s.length - 1)
-  firstDigit = (num / multiplesOfTen).floor
-  remainingDigits = expandedForm(num - multiplesOfTen * firstDigit)
+  multiples_of_ten = 10**(num.to_s.length - 1)
+  first_digit = (num / multiples_of_ten).floor
+  remaining_digits = expandedForm(num - multiples_of_ten * first_digit)
 
-  return "#{multiplesOfTen} x #{firstDigit}" if remainingDigits == "0"
+  return "#{multiples_of_ten} x #{first_digit}" if remaining_digits == "0"
 
-  "#{multiplesOfTen} x #{firstDigit} + #{remainingDigits}"
+  "#{multiples_of_ten} x #{first_digit} + #{remaining_digits}"
 end
 
 puts expandedForm(8); # 印出 8
@@ -21,3 +21,5 @@ puts expandedForm(148); # 印出 100 x 1 + 10 x 4 + 8
 puts expandedForm(1450); # 印出 1000 x 1 + 100 x 4 + 10 x 5
 puts expandedForm(60308); # 印出 10000 x 6 + 100 x 3 + 8
 
+# solving steps:
+# 判斷是否是個位數，如果是則回傳自己；接著針對每個位數做處理，將 10 的次方倍數與首位數字抓出來，接著再針對剩餘的位數做相同的步驟，直到 remaining_digits 被拆分為個位數為止。如果 remaining_digits 是 0，代表沒有非 0 的數字需要處理，則直接回傳 "#{multiples_of_ten} x #{first_digit}" 。
