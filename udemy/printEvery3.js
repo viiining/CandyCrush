21343b99b6315bf5c310a451b6146e9fae4256f5
Zinni Chang <czinni853@gmail.com>
2023-11-12 17:29:04 +0800

    [practice]: practice udemy js printEvery3().

---
+++ udemy/printEvery3.js
@@ -0,0 +1,17 @@
+// Method 1 (for loop)
+function printEvery3() {
+  for (let i = 1; i <= 88; i += 3) {
+    console.log(i);
+  }
+}
+printEvery3();
+
+// Method 2 (while loop)
+function printEvery3() {
+  let i = 1;
+  while (i <= 88) {
+    console.log(i);
+    i += 3;
+  }
+}
+printEvery3();
