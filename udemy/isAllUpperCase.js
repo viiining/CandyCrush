5a1b4c9671c3e4e74da3522d33a5c0373c86a3d6
Zinni Chang <czinni853@gmail.com>
2023-11-13 20:35:56 +0800

    [practice]: practice udemy js isAllUpperCase().

---
+++ udemy/isAllUpperCase.js
@@ -0,0 +1,24 @@
+function isAllUpperCase(str) {
+  if (str.length == 0) {
+    return false;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != str[i].toUpperCase()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+function isAllUpperCase(str) {
+  if (str.length == 0) {
+    return false;
+  }
+
+  return str == str.toUpperCase();
+}
+
+console.log(isAllUpperCase("ABCD")); // returns true
+console.log(isAllUpperCase("")); // returns false
+console.log(isAllUpperCase("ABCDEFGHIJKLm")); // returns false
